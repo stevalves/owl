df06bb5b4440b54172a8335ef20118312cc05ca1
stevalves <ealves1710@hotmail.com>
2023-08-23 20:36:35 -0300

    feat/finishing v1

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,9 +4,17 @@ interface iFooter {}
 
 const Footer = ({}: iFooter) => {
   return (
-    <footer className="flex h-24 w-full scale-100 items-center justify-center bg-forest-600 px-8 gap-6">
+    <footer className="flex h-24 w-full scale-100 items-center justify-center gap-6 bg-forest-600 px-8">
       <Logo />
-      <p className="text-forest-50 text-xl">2023 v1</p>
+      <p className="text-xl text-forest-50">
+        2023 v1|
+        <a
+          className="hover:underline"
+          href="https://my-portfolio-virid-sigma-80.vercel.app/"
+        >
+          beta
+        </a>
+      </p>
       <Logo />
     </footer>
   );
