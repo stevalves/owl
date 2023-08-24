8b16598315120c5261ff9f1ae006669be27a6b63
stevalves <ealves1710@hotmail.com>
2023-08-24 10:27:55 -0300

    fix/FooterScale

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ interface iFooter {}
 
 const Footer = ({}: iFooter) => {
   return (
-    <footer className="flex h-24 w-full scale-100 items-center justify-center gap-6 bg-forest-600 px-8">
+    <footer className="flex h-24 w-full items-center justify-center gap-6 bg-forest-600 px-8">
       <Logo />
       <p className="text-xl text-forest-50">
         2023 v1|
